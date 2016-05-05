5627976a89e5842df9b6fef8ba2c200b56ba63fb
cperez <cperez@madmobile.com>
2016-05-04 22:32:29 -0400

    adobe dtm module

--- NodeScholarship/NodeScholarship/app.js
+++ NodeScholarship/NodeScholarship/app.js
@@ -1,31 +1,72 @@
 "use strict";
-var _ = require('lodash');
-var cheerio = require('cheerio');
-var request = require('request');
-var fs = require('fs');
-var sleep = require('sleep');
-//var outfilestates = 'states.txt';
-var msg = '';
+var _ = require("lodash");
+var cheerio = require("cheerio");
+var request = require("request");
+var fs = require("fs");
+var sleep = require("sleep");
 var App;
 (function (App) {
     var IO;
     (function (IO) {
+        var fileSizeInMegabytes;
         var File = (function () {
             function File() {
             }
-            File.prototype.append = function (filePath, msg) {
+            File.append = function (filePath, msg) {
+                var _this = this;
                 fs.appendFile(filePath, msg, function (err) {
                     if (err) {
-                        this.append(filePath, msg);
+                        _this.append(filePath, msg);
                         return console.log(err);
                     }
                 });
             };
             ;
+            File.read = function (filePath) {
+                try {
+                    return fs.readFileSync(filePath, "utf8");
+                }
+                catch (e) {
+                    return "";
+                }
+            };
+            ;
+            File.exist = function (filePath) {
+                try {
+                    var stats = fs.lstatSync(filePath);
+                    return stats.isFile ? true : false;
+                }
+                catch (e) {
+                    return false;
+                }
+            };
+            File.clearContent = function (filePath) {
+                try {
+                    if (this.exist(filePath)) {
+                        fs.writeFile(filePath, "");
+                    }
+                    return (File.size(filePath) > 0);
+                }
+                catch (e) {
+                    return false;
+                }
+            };
+            File.size = function (filePath) {
+                try {
+                    var stats = fs.statSync(filePath);
+                    var fileSizeInBytes = stats["size"];
+                    return fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
+                }
+                catch (e) {
+                    return 0;
+                }
+            };
             return File;
         }());
         IO.File = File;
     })(IO = App.IO || (App.IO = {}));
+    var pageNumber;
+    var url;
     var ScholarShip;
     (function (ScholarShip) {
         var Parser = (function () {
@@ -39,19 +80,20 @@ var App;
             }
             Parser.prototype.getStatesList = function (callback) {
                 var stateUrls = [];
+                var msg = "";
                 request(url, function (error, response, html) {
                     if (!error && response.statusCode == 200) {
                         var $ = cheerio.load(html);
                         $('ul[class*="list left"]>li').each(function (i, element) {
-                            var title = $(element).find('a').text().trim();
-                            var link = $(element).find('a').attr('href');
-                            var completeUrl = '';
-                            if (title != '' && (title.indexOf('Scholarships') > 0)) {
-                                completeUrl = url + link.replace('./', '');
+                            var title = $(element).find("a").text().trim();
+                            var link = $(element).find("a").attr("href");
+                            var completeUrl = "";
+                            if (title !== "" && (title.indexOf("Scholarships") > 0)) {
+                                completeUrl = url + link.replace("./", "");
                                 if (pageNumber <= 0)
-                                    msg = title.split(' ')[0] + '|' + completeUrl + '\n';
+                                    msg = title.split(" ")[0] + "|" + completeUrl + "\r\n";
                                 else
-                                    msg = title.split(' ')[0] + '|' + completeUrl + '?page=' + pageNumber + '\n';
+                                    msg = title.split(" ")[0] + "|" + completeUrl + "?page=" + pageNumber + "\r\n";
                                 stateUrls.push({ State: title, Link: completeUrl });
                             }
                         });
@@ -63,42 +105,69 @@ var App;
             Parser.prototype.getStateScholarships = function (callback) {
                 var subStateUrls = [];
                 var ctr = 0;
-                _.each(this.model.stateList, function (item, index) {
+                var removed = this.model.stateList.splice(1, 1);
+                _.each(removed, function (item, index) {
                     var url = item.Link;
-                    var state = item.State.split(' ')[0];
+                    var state = item.State.split(" ")[0];
                     request(url, function (error, response, html) {
                         if (!error && response.statusCode == 200) {
-                            var $ = cheerio.load(html);
-                            var urls = $('tr[class="odd"]>td').find('a');
+                            var $_1 = cheerio.load(html);
+                            var urls = $_1('tr[class="odd"]>td').find("a");
                             if (urls.length > 0) {
                                 _.each(urls, function (stateChildSubUrl, index) {
                                     ctr++;
-                                    var suburl = url + stateChildSubUrl.attribs.href.replace('./', '');
-                                    //subStateUrls.push({ State: state, Link: suburl });                                  
-                                    sleep.usleep(30000);
-                                    console.log(ctr + '- ' + suburl);
+                                    var suburl = url + stateChildSubUrl.attribs.href.replace("./", "");
+                                    subStateUrls.push({ State: state, Link: suburl });
+                                    sleep.usleep(100000);
+                                    console.log(ctr + "- " + suburl);
                                 });
                             }
+                            callback(subStateUrls);
                         }
                         else {
-                            var msg = 'Error: ' + url + '[' + error + ']: ';
+                            var msg = "Error: " + url + "[" + error + "]: ";
                             console.log(msg);
                         }
                     }, state);
                 });
             };
+            Parser.prototype.reIndexData = function (jsonArray) {
+                _.each(jsonArray, function (item, index) {
+                    item.id = index;
+                });
+                return jsonArray;
+            };
             return Parser;
         }());
         ScholarShip.Parser = Parser;
     })(ScholarShip = App.ScholarShip || (App.ScholarShip = {}));
-    var pageNumber = (process.argv[2] || 0);
-    var url = 'https://www.schoolsoup.com/scholarship-directory/state/';
+    pageNumber = (process.argv[2] || 0);
+    url = 'https://www.schoolsoup.com/scholarship-directory/state/';
     var app = new App.ScholarShip.Parser(url, pageNumber);
     app.getStatesList(function (result) {
         if (result.length > 0)
             app.model.stateList = result;
         app.getStateScholarships(function (statescholarship) {
-            app.model.stateScholarships = app.model.stateScholarships.concat(statescholarship);
+            var filePath = "./data.json";
+            var strJson = "";
+            var objScholarship;
+            if (IO.File.exist((filePath))) {
+                if (IO.File.size(filePath) > 0) {
+                    objScholarship = JSON.parse(fs.readFileSync(filePath, "utf8"));
+                    objScholarship = objScholarship.concat(statescholarship);
+                }
+                else {
+                    objScholarship = statescholarship;
+                }
+                objScholarship = app.reIndexData(objScholarship);
+                strJson = JSON.stringify(objScholarship, null, 2);
+            }
+            else {
+                objScholarship = app.reIndexData(statescholarship);
+                strJson = JSON.stringify(objScholarship, null, 2);
+            }
+            IO.File.clearContent(filePath);
+            IO.File.append(filePath, strJson);
         });
     });
 })(App || (App = {}));
--- NodeScholarship/NodeScholarship/app.ts
+++ NodeScholarship/NodeScholarship/app.ts
@@ -1,64 +1,92 @@
-﻿
-import _ = require('lodash');
-import cheerio = require('cheerio');
-import request = require('request');
-import fs = require('fs');
-var sleep = require('sleep');
-
-
-//var outfilestates = 'states.txt';
-var msg = '';
+﻿import _ = require("lodash");
+import cheerio = require("cheerio");
+import request = require("request");
+import fs = require("fs");
+var sleep = require("sleep");
 namespace App {
     interface IStateList {
         (getStatesList: any[]): any;
     }
-
     interface IStateScholarships {
         (getStateScholarships: any[]): any;
     }
-
     interface IStateScholarshipDetails {
         statename: string;
         getScholarshipByState: (urls: any[], state: string) => void;
     }
-
     export namespace IO {
+        var fileSizeInMegabytes: number;
         export class File {
             constructor() { }
-            append(filePath: string, msg: string) {
-                fs.appendFile(filePath, msg, function (err: any) {
+            static append(filePath: string, msg: string) {
+                fs.appendFile(filePath, msg, (err: any) => {
                     if (err) {
                         this.append(filePath, msg);
                         return console.log(err);
                     }
                 });
             };
+            static read(filePath: string) {
+                try {
+                    return fs.readFileSync(filePath, "utf8");
+                } catch (e) {
+                    return "";
+                }                 
+            };
+            static exist(filePath: string) {
+                try {
+                    const stats: any = fs.lstatSync(filePath);
+                    return stats.isFile ? true : false;
+                } catch (e) {
+                    return false;
+                }
+            }
+            static clearContent(filePath: string) {
+                try {
+                    if (this.exist(filePath)) {
+                        fs.writeFile(filePath, "");                        
+                    }
+                    return (File.size(filePath) > 0);
+                } catch (e) {
+                    return false;
+                }
+            }
+            static size(filePath: string) {
+                try {
+                    const stats = fs.statSync(filePath);
+                    const fileSizeInBytes = stats["size"];
+                    return fileSizeInMegabytes = fileSizeInBytes / 1000000.0;                   
+                } catch (e) {
+                    return 0;
+                } 
+            }
         }
     }
-
+    var pageNumber: number;
+    var url: string;
     export namespace ScholarShip {
         export class Parser {
             model = {
-                stateList: <any[]>[],
-                stateScholarships: <any[]>[]
-
-            }
+                stateList: [] as any[],
+                stateScholarships: [] as any[]
+            };
             constructor(public url: string, public pageNumber: number) { }
             getStatesList(callback: IStateList): void {
                 var stateUrls: any[] = [];
-                request(url, function (error: any, response: any, html: any) {
+                var msg = "";
+                request(url, (error: any, response: any, html: any) => {
                     if (!error && response.statusCode == 200) {
                         var $ = cheerio.load(html);
-                        $('ul[class*="list left"]>li').each(function (i, element) {
-                            var title = $(element).find('a').text().trim();
-                            var link = $(element).find('a').attr('href');
-                            var completeUrl = '';
-                            if (title != '' && (title.indexOf('Scholarships') > 0)) {
-                                completeUrl = url + link.replace('./', '');
+                        $('ul[class*="list left"]>li').each((i, element) => {
+                            var title = $(element).find("a").text().trim();
+                            var link = $(element).find("a").attr("href");
+                            var completeUrl = "";
+                            if (title !== "" && (title.indexOf("Scholarships") > 0)) {
+                                completeUrl = url + link.replace("./", "");
                                 if (pageNumber <= 0)
-                                    msg = title.split(' ')[0] + '|' + completeUrl + '\n';
+                                    msg = title.split(" ")[0] + "|" + completeUrl + "\r\n";
                                 else
-                                    msg = title.split(' ')[0] + '|' + completeUrl + '?page=' + pageNumber + '\n';
+                                    msg = title.split(" ")[0] + "|" + completeUrl + "?page=" + pageNumber + "\r\n";
                                 stateUrls.push({ State: title, Link: completeUrl });
                             }
                         });
@@ -69,26 +97,27 @@ namespace App {
             getStateScholarships(callback: IStateScholarships): void {
                 var subStateUrls: any[] = [];
                 var ctr = 0;
-                _.each(this.model.stateList, function (item, index) {
+                var removed = this.model.stateList.splice(1, 1);
+                _.each(removed, (item, index) => {
                     var url = item.Link;
-                    var state = item.State.split(' ')[0];
-                    request(url, function (error: any, response: any, html: any) {
+                    var state = item.State.split(" ")[0];
+                    request(url, (error: any, response: any, html: any) => {
                         if (!error && response.statusCode == 200) {
-                            var $ = cheerio.load(html);
-                            var urls = $('tr[class="odd"]>td').find('a');
+                            const $ = cheerio.load(html);
+                            const urls = $('tr[class="odd"]>td').find("a");
                             if (urls.length > 0) {
-                                _.each(urls, function (stateChildSubUrl, index) {
+                                _.each(urls, (stateChildSubUrl, index) => {
                                     ctr++;
-                                    var suburl = url + stateChildSubUrl.attribs.href.replace('./', '')
-                                    //subStateUrls.push({ State: state, Link: suburl });                                  
-                                    sleep.usleep(30000);
-                                    console.log(ctr + '- ' + suburl);
+                                    var suburl = url + stateChildSubUrl.attribs.href.replace("./", "");
+                                    subStateUrls.push({ State: state, Link: suburl });                                  
+                                    sleep.usleep(100000);
+                                    console.log(ctr + "- " + suburl);
                                 });
                             }
-                            //callback(subStateUrls);                           
+                            callback(subStateUrls);                           
                         }
                         else {
-                            var msg = 'Error: ' + url + '[' + error + ']: ';
+                            const msg = `Error: ${url}[${error}]: `;
                             console.log(msg);
                         }
                     }, state);
@@ -96,21 +125,44 @@ namespace App {
                 });
 
             }
+
+            reIndexData(jsonArray: any[]): any[] {
+                _.each(jsonArray, (item, index) => {
+                     item.id = index;
+                });
+                return jsonArray;
+            }
         }
     }
-    var pageNumber: number = <number>(process.argv[2] || 0);
-    var url = 'https://www.schoolsoup.com/scholarship-directory/state/';
+
+    pageNumber = <number>(process.argv[2] || 0);
+    url = 'https://www.schoolsoup.com/scholarship-directory/state/';
     var app = new App.ScholarShip.Parser(url, pageNumber);
     app.getStatesList((result: any[]): void => {
         if (result.length > 0)
             app.model.stateList = result;
 
         app.getStateScholarships((statescholarship: any[]): void => {
-            app.model.stateScholarships = app.model.stateScholarships.concat(statescholarship);
-            
+            var filePath = "./data.json";
+            var strJson = "";
+            var objScholarship: any[];
+            if (IO.File.exist((filePath))) {
+                if (IO.File.size(filePath) > 0) {
+                    objScholarship = JSON.parse(fs.readFileSync(filePath, "utf8"));                   
+                    objScholarship = objScholarship.concat(statescholarship);                   
+                } else {
+                    objScholarship = statescholarship;
+                }
+                objScholarship = app.reIndexData(objScholarship);                                          
+                strJson = JSON.stringify(objScholarship, null, 2);
+            } else {
+                objScholarship = app.reIndexData(statescholarship);
+                strJson = JSON.stringify(objScholarship, null, 2);
+            }
 
+            IO.File.clearContent(filePath);   
+            IO.File.append(filePath,strJson);
         });
-
     });
 
 }
